48e03d604480bedf8dc2922426f5ca515e59433c
daddy-rizzler <daddy_rizzler@outlook.com>
2025-03-28 14:51:49 -0400

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -2,7 +2,7 @@
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 echo Successfully applied wallpaper.
-reg add "HKEY_USERS\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
-reg add "HKEY_USERS\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
+reg add "HKEY_USERS\.DEFAULT\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+reg add "HKEY_USERS\.DEFAULT\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 echo Successfully applied wallpaper for all users.
 RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
