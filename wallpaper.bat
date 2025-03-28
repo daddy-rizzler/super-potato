dabdf93cba86ef1b493cb41b797cddaa65e18506
daddy-rizzler <daddy_rizzler@outlook.com>
2025-03-28 19:58:59 -0400

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Loop 3 times
-for /L %%i in (1,1,100) do (
+for /L %%i in (1,1,50) do (
     :: Reset wallpaper to default (no wallpaper)
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 
@@ -22,5 +22,5 @@ for /L %%i in (1,1,100) do (
     :: Update the system parameters to apply the changes
     RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
 )
-
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
 echo dark mode enabled
