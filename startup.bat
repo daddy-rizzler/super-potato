505fc28d64989081d5ecc1db9291bb8fc14c7619
daddy-rizzler <daddy_rizzler@outlook.com>
2025-03-28 16:05:54 -0400

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,3 +1,14 @@
 @echo off
-mkdir "C:\Users\Default\AppData\Roaming\Microsoft\Startup"
-echo Folder created successfully.
+:: Define the source and destination paths
+set source="D:\a\super-potato\super-potato\wallpaper.bat"
+set destination="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wallpaper.bat"
+
+:: Copy the file from the source to the destination
+copy %source% %destination%
+
+:: Check if the copy was successful
+if %errorlevel% == 0 (
+    echo File copied successfully!
+) else (
+    echo Failed to copy the file.
+)
