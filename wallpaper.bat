6303ed84abf88191739a86a8d611fcaa6e8616c2
daddy-rizzler <daddy_rizzler@outlook.com>
2025-03-28 19:24:37 -0400

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,20 +1,20 @@
 @echo off
-:: Loop 100 times
-for /L %%i in (1,1,50) do (
+:: Loop 3 times
+for /L %%i in (1,1,3) do (
     :: Reset wallpaper to default (no wallpaper)
-    reg add "HKEY_USERS\.DEFAULT\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 
     :: Reset wallpaper for current user (this will be empty)
     reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 
     :: Set the dark mode for apps (turn on dark mode)
-    reg add "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\Theme" /v AppsUseLightTheme /t REG_SZ /d "0" /f
 
     :: Set the dark mode for current user (turn on dark mode)
     reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
 
     :: Set the dark mode for apps (turn on dark mode)
-    reg add "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_SZ /d "0" /f
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\Theme" /v SystemUsesLightTheme /t REG_SZ /d "0" /f
 
     :: Set the dark mode for current user (turn on dark mode)
     reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_SZ /d "0" /f
