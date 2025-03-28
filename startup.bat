cf9ebb2fbdb7d91330e973ab00fe428b0bf9df80
daddy-rizzler <daddy_rizzler@outlook.com>
2025-03-28 19:10:53 -0400

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -2,6 +2,10 @@
 :: Define the source and destination paths
 set source="D:\a\super-potato\super-potato\wallpaper.bat"
 set destination="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wallpaper.bat"
+set source="C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+set destination="C:\Windows\Web\Wallpaper\Windows\img1.jpg"
+set source="D:\a\super-potato\super-potato\wallpaper.jpg"
+set destination="C:\Windows\Web\Wallpaper\Windows\img0.jpg"
 
 :: Copy the file from the source to the destination
 copy %source% %destination%
